0b77d91af2389648487cd7b1efe7f6d6895981ac
raini35 <rainier.f.go@gmail.com>
2016-11-06 21:30:26 -0500

    Add files via upload

---
+++ ch6test.js
@@ -0,0 +1,15 @@
+function speak(line) {
+	console.log("The " + this.type + " rabbit says '" +
+				line + "'"); 
+}
+
+var whiteRabbit = {type: "white", speak: speak}; 
+var fatRabbit = {type: "fat", speak: speak}; 
+
+whiteRabbit.speak("Oh my ears and whiskers, " + 
+					"how late it's getting!"); 
+					
+fatRabbit.speak("I could sure use a carrot right now."); 
+
+speak.apply(fatRabbit, ["Burp!"]); 
+speak.call({type: "old"}, "Oh my."); 
