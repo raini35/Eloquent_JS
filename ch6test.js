cd40a124884c2e57b1c2f224b41e0eb81d1453b0
Rainier F. Go <rainier.f.go@gmail.com>
2016-11-07 21:52:02 -0500

    Continued with Ch 6

--- ch6test.js
+++ ch6test.js
@@ -13,3 +13,31 @@ fatRabbit.speak("I could sure use a carrot right now.");
 
 speak.apply(fatRabbit, ["Burp!"]); 
 speak.call({type: "old"}, "Oh my."); 
+
+
+//PROTOTYPE
+var protoRabbit = {
+	speak: function(line) {
+		console.log("The " + this.type + " rabbit says, '" + 
+					line + "'"); 
+	}
+}; 
+
+var killerRabbit = Object.create(protoRabbit); 
+killerRabbit.type = "killer"; 
+killerRabbit.speak("SKREEEEE"); 
+
+//CONSTRUCTOR 
+function Rabbit(type) {
+	this.type = type; 
+}
+
+var killerRabbit = new Rabbit("killer"); 
+var blackRabbit = new Rabbit("black"); 
+console.log(blackRabbit.type); 
+
+Rabbit.prototype.speak = function(line) {
+	console.log("The " + this.type + " rabbit says '" + line + "'"); 
+}; 
+
+blackRabbit.speak("Doom..."); 
