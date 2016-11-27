5b0cac427a8a64db2e80e172c3785739da9262a2
Rainier F. Go <rainier.f.go@gmail.com>
2016-11-26 21:47:25 -0500

    Learned about forEach

--- ch5_redo.js
+++ ch5_redo.js
@@ -28,3 +28,34 @@ forEach(numbers, function(number) {
 }); 
 
 console.log(sum); 
+
+
+//Array already has a forEach method attached to it
+
+/*
+WITHOUT USING FOR EACH 
+function gatherCorrelations(journal) {
+	var phis = {}; 
+	for(var entry = 0; entry < journal.length; entry++) {
+		var events = journal[entry].events; 
+		for(var i = 0; i < events.length; i++) {
+			var event = events[i]; 
+			if(!(event in phis))
+				phis[event] = phi(tableFor(event, journal)); 
+		}
+	}
+	return phis; 
+}
+
+USING FOR EACH 
+function gatherCorrelations2(journal) {
+	var phis = {}; 
+	journal.forEach(function(entry) {
+		entry.events.forEach(function(event) {
+			if(!(event in phis)) 
+				phis[event] = phi(tableFor(event, journal)); 
+		}); 
+	}); 
+	return phis; 
+}
+*/
