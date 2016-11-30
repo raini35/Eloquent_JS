ed7c4247c635d43397eff22560796d1058f24bbd
Rainier F. Go <rainier.f.go@gmail.com>
2016-11-29 23:33:55 -0500

    Ch6 Notes on History of Objects

---
+++ ch6notes.js
@@ -0,0 +1,16 @@
+/*
+HISTORY OF OBJECTS
+
+Complexity can be made manageable by separating the entire program into 
+small compartments isolated from each other. 
+
+An object is a hard shell that hides the gooey complexity inside it and 
+instead offers methods that present an interface through which the object
+is used. 
+
+The idea is that the interface allows the user to work with the object 
+without knowing what's going inside.
+
+Encapsulation - distinguishing between internal and external interface
+
+*/
