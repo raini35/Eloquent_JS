f6b3b1c4a827ecfbda7ae2b5848033d91314c848
Rainier F. Go <rainier.f.go@gmail.com>
2016-12-02 17:11:32 -0500

    Finished Ch6 Notes and added Ch6 Exercise

--- ch6notes.js
+++ ch6notes.js
@@ -1,3 +1,4 @@
+var MOUNTAINS = require('./mountains.js');
 /*
 HISTORY OF OBJECTS
 
@@ -178,3 +179,163 @@ var map = Object.create(null);
 map["pizza"] = 0.069; 
 console.log("toString" in map); 
 console.log("pizza" in map); 
+
+//POLYMORPHISM 
+//Polymorphic code can work with alues of different shapes, as long as they 
+//support the interface it expects. 
+
+//LAYOUT OUT A TABLE
+function rowHeights(rows) {
+  return rows.map(function(row) {
+    return row.reduce(function(max, cell) {
+      return Math.max(max, cell.minHeight());
+    }, 0);
+  });
+}
+
+function colWidths(rows) {
+  return rows[0].map(function(_, i) {
+    return rows.reduce(function(max, row) {
+      return Math.max(max, row[i].minWidth());
+    }, 0);
+  });
+}
+
+function drawTable(rows) {
+  var heights = rowHeights(rows);
+  var widths = colWidths(rows);
+
+  function drawLine(blocks, lineNo) {
+    return blocks.map(function(block) {
+      return block[lineNo];
+    }).join(" ");
+  }
+
+  function drawRow(row, rowNum) {
+    var blocks = row.map(function(cell, colNum) {
+      return cell.draw(widths[colNum], heights[rowNum]);
+    });
+    return blocks[0].map(function(_, lineNo) {
+      return drawLine(blocks, lineNo);
+    }).join("\n");
+  }
+
+  return rows.map(drawRow).join("\n");
+}
+
+function repeat(string, times) {
+  var result = "";
+  for (var i = 0; i < times; i++)
+    result += string;
+  return result;
+}
+
+function TextCell(text) {
+  this.text = text.split("\n");
+}
+TextCell.prototype.minWidth = function() {
+  return this.text.reduce(function(width, line) {
+    return Math.max(width, line.length);
+  }, 0);
+};
+TextCell.prototype.minHeight = function() {
+  return this.text.length;
+};
+TextCell.prototype.draw = function(width, height) {
+  var result = [];
+  for (var i = 0; i < height; i++) {
+    var line = this.text[i] || "";
+    result.push(line + repeat(" ", width - line.length));
+  }
+  return result;
+};
+
+function UnderlinedCell(inner) {
+  this.inner = inner;
+}
+UnderlinedCell.prototype.minWidth = function() {
+  return this.inner.minWidth();
+};
+UnderlinedCell.prototype.minHeight = function() {
+  return this.inner.minHeight() + 1;
+};
+UnderlinedCell.prototype.draw = function(width, height) {
+  return this.inner.draw(width, height - 1)
+    .concat([repeat("-", width)]);
+};
+
+function RTextCell(text) {
+	TextCell.call(this, text); 
+}
+RTextCell.prototype = Object.create(TextCell.prototype); 
+RTextCell.prototype.draw = function(width, height) {
+	var result = []; 
+	for(var i = 0; i < height; i++) {
+		var line = this.text[i] || ""; 
+		result.push(repeat(" ", width - line.length) + line); 
+	}
+	return result; 
+}; 
+
+function dataTable(data) {
+  var keys = Object.keys(data[0]);
+  var headers = keys.map(function(name) {
+    return new UnderlinedCell(new TextCell(name));
+  });
+  var body = data.map(function(row) {
+    return keys.map(function(name) {
+    	var value = row[name]; 
+    	if(typeof value == "number") 
+    		return new RTextCell(String(value)); 
+    	else 
+      		return new TextCell(String(row[name]));
+    });
+  });
+  return [headers].concat(body);
+}
+
+var rows = [];
+for (var i = 0; i < 8; i++) {
+   var row = [];
+   for (var j = 0; j < 2; j++) {
+     if ((j + i) % 2 == 0)
+       row.push(new TextCell("##"));
+     else
+       row.push(new TextCell("  "));
+   }
+   rows.push(row);
+}
+console.log(drawTable(rows));
+console.log(drawTable(dataTable(MOUNTAINS)));
+
+
+//GETTERS AND SETTERS 
+var pile = {
+	elements: ["eggshell", "orange peel", "worm"], 
+	get height() {
+		return this.elements.length; 
+	}, 
+	set height(value) {
+		console.log("Ignoring attempt to set height to", value); 
+	}
+};
+
+console.log(pile.height); 
+
+pile.height = 100;
+
+Object.defineProperty(TextCell.prototype, "heightProp", { 
+	get: function() { return this.text.length; }
+});
+
+var cell = new TextCell("no\nway");
+console.log(cell.heightProp); 
+
+cell.heightProp = 100; 
+console.log(cell.heightProp);
+
+//THE INSTANCE OF OPERATOR 
+console.log(new RTextCell("A") instanceof RTextCell); 
+console.log(new RTextCell("A") instanceof TextCell); 
+console.log(new TextCell("A") instanceof RTextCell); 
+console.log([1] instanceof Array);
