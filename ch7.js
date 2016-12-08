59f19ce2e514e8093e2ff4b507326ecee60bda70
Rainier F. Go <rainier.f.go@gmail.com>
2016-12-08 01:56:49 -0500

    Finished up until animateWorld

--- ch7.js
+++ ch7.js
@@ -52,12 +52,31 @@ Grid.prototype.set = function(vector, value) {
 	this.space[vector.x + this.width * vector.y] = value; 
 };
 
-
+Grid.prototype.forEach = function(f, context) {
+  for (var y = 0; y < this.height; y++) {
+    for (var x = 0; x < this.width; x++) {
+      var value = this.space[x + y * this.width];
+      if (value != null)
+        f.call(context, value, new Vector(x, y));
+    }
+  }
+};
 /**********************************************************************
 *
 *							CRITTER CLASS
 *
 **********************************************************************/
+var directions = {
+  "n":  new Vector( 0, -1),
+  "ne": new Vector( 1, -1),
+  "e":  new Vector( 1,  0),
+  "se": new Vector( 1,  1),
+  "s":  new Vector( 0,  1),
+  "sw": new Vector(-1,  1),
+  "w":  new Vector(-1,  0),
+  "nw": new Vector(-1, -1)
+};
+
 function randomElement(array) {
 	return array[Math.floor(Math.random() * array.length)]; 
 }
@@ -99,6 +118,86 @@ function World(map, legend) {
 	});
 }	
 
+function charFromElement(element) {
+	if(element == null) 
+		return " "; 
+	else 
+		return element.originChar; 
+}
+
+World.prototype.toString = function() {
+	var output = ""; 
+	for(var y = 0; y < this.grid.height; y++) {
+		for(var x = 0; x < this.grid.width; x++) {
+			var element = this.grid.get(new Vector(x, y)); 
+			output += charFromElement(element); 
+		}
+		output += "\n"; 
+	}
+	
+	return output; 
+}; 
+
+World.prototype.turn = function() {
+  var acted = [];
+  this.grid.forEach(function(critter, vector) {
+    if (critter.act && acted.indexOf(critter) == -1) {
+      acted.push(critter);
+      this.letAct(critter, vector);
+    }
+  }, this);
+};
+
+World.prototype.letAct = function(critter, vector) {
+  var action = critter.act(new View(this, vector));
+  if (action && action.type == "move") {
+    var dest = this.checkDestination(action, vector);
+    if (dest && this.grid.get(dest) == null) {
+      this.grid.set(vector, null);
+      this.grid.set(dest, critter);
+    }
+  }
+};
+
+World.prototype.checkDestination = function(action, vector) {
+  if (directions.hasOwnProperty(action.direction)) {
+    var dest = vector.plus(directions[action.direction]);
+    if (this.grid.isInside(dest))
+      return dest;
+  }
+};
+
+function Wall() {}
+
+/**********************************************************************
+*
+*							CRITTER CLASS
+*
+**********************************************************************/
+
+function View(world, vector) {
+  this.world = world;
+  this.vector = vector;
+}
+View.prototype.look = function(dir) {
+  var target = this.vector.plus(directions[dir]);
+  if (this.world.grid.isInside(target))
+    return charFromElement(this.world.grid.get(target));
+  else
+    return "#";
+};
+View.prototype.findAll = function(ch) {
+  var found = [];
+  for (var dir in directions)
+    if (this.look(dir) == ch)
+      found.push(dir);
+  return found;
+};
+View.prototype.find = function(ch) {
+  var found = this.findAll(ch);
+  if (found.length == 0) return null;
+  return randomElement(found);
+};
 /**********************************************************************
 *
 *							MAIN CLASS
@@ -118,3 +217,12 @@ grid.set(new Vector(1, 1), "X");
 console.log(grid.get(new Vector(1, 1)));
 console.log(grid);
 
+var world = new World(plan, {"#": Wall,
+                             "o": BouncingCritter});
+console.log(world.toString());
+/*for (var i = 0; i < 5; i++) {
+  world.turn();
+  console.log(world.toString());
+}*/
+
+animateWorld(world);
