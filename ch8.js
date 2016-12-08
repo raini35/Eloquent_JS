fba03bd0d63dc21eaff7d42304e7f64413a8f303
Rainier F. Go <rainier.f.go@gmail.com>
2016-12-08 02:44:35 -0500

    Finished error handling/debugging

--- ch8.js
+++ ch8.js
@@ -21,3 +21,38 @@ function testVector() {
 }
 
 console.log(testVector()); 
+
+function promptNumber(question) {
+	var result = Number(prompt(question, "")); 
+	if(isNaN(result)) return null; 
+	else return result; 
+}
+
+console.log(promptNumber("How many trees do you see?"));
+
+function promptDirection(question) {
+  var result = prompt(question, "");
+  if (result.toLowerCase() == "left") return "L";
+  if (result.toLowerCase() == "right") return "R";
+  throw new Error("Invalid direction: " + result);
+}
+
+function look() {
+  if (promptDirection("Which way?") == "L")
+    return "a house";
+  else
+    return "two angry bears";
+}
+
+try {
+  console.log("You see", look());
+} catch (error) {
+  console.log("Something went wrong: " + error);
+}
+
+function InputError(message) {
+  this.message = message;
+  this.stack = (new Error()).stack;
+}
+InputError.prototype = Object.create(Error.prototype);
+InputError.prototype.name = "InputError";
