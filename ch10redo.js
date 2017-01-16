f5d88fa52824f27c29054fd89558a9c7f08723ca
Rainier F. Go <rainier.f.go@gmail.com>
2017-01-15 22:18:34 -0500

    ch10redo.js - Evaluating data as code

--- ch10redo.js
+++ ch10redo.js
@@ -1,6 +1,28 @@
-//WHY MODULES HELP 
+//OBJECTS AS INTERFACES
+var weekDay = function() {
+	var names = ["Sunday", "Monday", "Tuesday", "Wednesday",
+               "Thursday", "Friday", "Saturday"];
+	
+	return {
+		name: function(number) { return names[number]; }, 
+		number: function(name) { return names.indexOf(name); }
+	}; 
+}(); 
 
-//NAMESPACING 
-//Most languages have a scope level between 
-//global and local 
-//Javascript does not have that 
+
+(function(exports) {
+	var names = ["Sunday", "Monday", "Tuesday", "Wednesday",
+               "Thursday", "Friday", "Saturday"];
+    
+    exports.name = function(number) {
+    	return names[number]; 
+    }; 
+    
+    exports.number = function(name) {
+    	return names.indexOf(name); 
+    }; 
+})(this.weekDay = {}); 
+
+//EVALUATING DATA AS CODE 
+var plusOne = new Function("n", "return n + 1;"); 
+console.log(plusOne(4)); 
